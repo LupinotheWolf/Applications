14d3b03c84dd7ffa128f0be294bd70a17c82cbdd
LupinotheWolf <74966209+LupinotheWolf@users.noreply.github.com>
2021-05-11 20:13:21 +0100

    fxd some HTML

--- music/views.py
+++ music/views.py
@@ -2,12 +2,16 @@ from django.shortcuts import render
 from .models import *
 
 def index(request):
+    site_hits = request.session.get('sit_hits', 1)
+    request.session['site_hits'] = site_hits + 1
+
     all_songs = Song.objects.all()
     all_artists = Artist.objects.all()
 
     context = {
         'all_songs': all_songs,
         'all_artists': all_artists,
+        'site_hits': site_hits,
     }
 
     return render(request, 'musicindex.html', context = context)
